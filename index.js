ecf0a1af243edc62d2cb9bb7d420c371e45ec240
kazitihum <kazitihum@gmail.com>
2019-05-20 13:13:46 +0600

    2 files modified

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import axios from 'axios';
 import Hello from './Hello';
 import './style.css';
 
@@ -7,17 +8,38 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: 'React'
+      name: 'React',
+      video: []
     };
+  }
+
+  componentWillMount() {
+    setInterval(() => {
+      this.VideoList();
+    }, 50000)
+  }
+
+  async VideoList() {
+    axios.get('https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=25&playlistId=PLq71IJk8mCV4-DqsZ7W6zRS7uzKOmmT8j&key=AIzaSyBzqHmkT5G4Ja6wcG-lPgqYwuGDulisc5A')
+      .then(function (response) {
+        this.setState({ video: response.results });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
   render() {
     return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
+      <div id="layout-content" className="layout-content-wrapper">
+        <div className="panel-list">
+          {this.state.video.map((item) => {
+            return (
+              <h1>{item.snippet.title}</h1>
+
+            )
+          })}
+        </div>
       </div>
     );
   }
