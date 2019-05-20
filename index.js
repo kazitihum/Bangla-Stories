c8e901784e64945cc1c1859a892429709ad541ca
kazitihum <kazitihum@gmail.com>
2019-05-20 14:57:18 +0600

    First Success

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import axios from 'axios';
-import Hello from './Hello';
+// import Hello from './Hello';
 import './style.css';
+import ReactPlayer from 'react-player';
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+  super(props);
     this.state = {
-      name: 'React',
-      video: []
+      video: [],
     };
   }
 
-  componentWillMount() {
-    setInterval(() => {
-      this.VideoList();
-    }, 50000)
+  componentDidMount() {
+  this.VideoList();
   }
 
-  async VideoList() {
-    axios.get('https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=25&playlistId=PLq71IJk8mCV4-DqsZ7W6zRS7uzKOmmT8j&key=AIzaSyBzqHmkT5G4Ja6wcG-lPgqYwuGDulisc5A')
-      .then(function (response) {
-        this.setState({ video: response.results });
+  VideoList() {
+    axios.get('https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=1&playlistId=PLq71IJk8mCV4-DqsZ7W6zRS7uzKOmmT8j&key=AIzaSyAFUNYmE1gfydRFrlb3Q05gXlPSgQmiY6I')
+      .then(response => {
+        this.setState({ video: response.data.items });
       })
       .catch(function (error) {
         console.log(error);
@@ -33,13 +31,11 @@ class App extends Component {
     return (
       <div id="layout-content" className="layout-content-wrapper">
         <div className="panel-list">
-          {this.state.video.map((item) => {
-            return (
-              <h1>{item.snippet.title}</h1>
-
-            )
-          })}
+          { this.state.video.map(item =>
+              <h1 key={item.id}>{item.snippet.title}</h1>
+          )}
         </div>
+        <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' playing />
       </div>
     );
   }
